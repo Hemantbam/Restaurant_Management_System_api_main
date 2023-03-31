2da7c9e714a92ac9e5d91ab81a147fb3ef854c51
Hemant Bam <hemantbam9865@gmail.com>
2023-03-31 22:56:55 +0545

    User authentication

--- src/handlers/user.js
+++ src/handlers/user.js
@@ -1,56 +1,35 @@
 import prisma from "../db.js";
-import { comparePassword, createJWT, hashPassword } from "../modules/auth.js";
+import { comparePassword, createJWT, hashPassword } from "../modules/auth.js"; // import several functions from external modules 
 
 export const signUp = async (req, res) => {
-  const user = await prisma.user.create({
+  const user = await prisma.user.create({  // prisma function create used to add the new user record to the database
     data: {
       username: req.body.username,
       email: req.body.email,
       restaurant_name: req.body.restaurant_name,
       phone_number: req.body.phone_number,
-      password: await hashPassword(req.body.password),
+      password: await hashPassword(req.body.password), // store the password into hascode into database
     },
   });
   const token = createJWT(user);
   res.json({ token });
 };
 
-export const signIn = async (req, res) => {
-  console.log(req.body.username);
+export const signIn = async (req, res) => {  // the function uses request and response parameter to handel sign in proces
+  console.log(req.body.username);               
   const user = await prisma.user.findUnique({
     where: {
       username: req.body.username,
     },
   });
 
-  const isValid = await comparePassword(req.body.password, user.password);
+  const isValid = await comparePassword(req.body.password, user.password);  // compare the user input password with database password
 
   if (!isValid) {
-    res.status(401);
+    res.status(401);  // response the 404 unauthorized message if isValid returns false
     res.json({ message: "NOPE" });
     return;
   }
   const token = createJWT(user);
   res.json({ token });
 };
-export const createFoodItem = async (req, res) => {
-  const user = await prisma.foodItem.create({
-    data: {
-      food_Name: req.body.foodname,
-      category: req.body.foodCategory,
-      Ingredient: req.body.foodIngredient,
-      price: req.body.foodPrice,
-      // photo_Src: req.body.photoSrc == null
-    },
-
-  });
-
-  res.json({ data:user  });
-};
-
-export const getFoodItem = async (req, res) => {
-  const user = await prisma.foodItem.findMany();
-
-  res.json({ data:user  });
-};
-
