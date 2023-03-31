73fa0e695466462756abd78b7c6f8ce5f27a0fb1
Hemant Bam <hemantbam9865@gmail.com>
2023-03-31 23:02:58 +0545

    Modification of server for each CRUD operation of foodList, Food Order List

--- src/server.js
+++ src/server.js
@@ -1,25 +1,40 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import { createFoodItem, getFoodItem, signIn, signUp } from "./handlers/user.js";
+import { signIn, signUp } from "./handlers/user.js";
+import { createFoodItem, getFoodItem, deleteFoodItem , updateFoodItem } from "./handlers/foodItem.js";
+import { createOrderList ,getOrderList, updateOrderList, delteeOrderList} from "./handlers/order.js";
 
 const app = express();
+
+//post - create
+//get - getting valuse from db
+//put - for updating value in db
+//delete - Deleting the data
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 
+// User Authentication 
+
 app.post("/signup", signUp);
 app.post("/signIN", signIn);
 
+// Food Item
+
 app.post("/createFoodItem", createFoodItem);
 app.get("/getFoodItem", getFoodItem);
-// app.put("/updateFoodItem", updateFoodItem);
+app.delete("/deleteFoodItem", deleteFoodItem);
+app.put("/updateFoodItem", updateFoodItem);
 
-//post - create
-//get - getting valuse from db
-//put - for updating value in db
-//delete - 
+//Order list
+app.post("/createOrderList",createOrderList);
+app.get("/getOrderList",getOrderList);
+app.put("/updateOrderList", updateOrderList);
+app.delete("/delteeOrderList", delteeOrderList);
+
+
 
 export default app;
