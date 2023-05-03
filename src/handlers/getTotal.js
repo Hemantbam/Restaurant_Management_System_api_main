1df5fb469a17aa521a5b0026b9f0b47a126854d1
Hemant Bam <hemantbam9865@gmail.com>
2023-05-03 22:06:09 +0545

    Get the Total Number of row data from different database table

---
+++ src/handlers/getTotal.js
@@ -0,0 +1,45 @@
+import prisma from "../db.js";
+
+export const getTotalEmployee = async (req, res) => {
+  try {
+    const count = await prisma.employee.count({
+      where: {
+        adminid: req.params.id,
+      },
+    });
+    res.json({ data: count });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const getTotalFoodItems = async (req, res) => {
+  try {
+    const count = await prisma.food_items.count({
+      where: {
+        adminid: req.params.id,
+      },
+    });
+
+    res.json({ data: count });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const getTotalSales = async (req, res) => {
+  try {
+    const count = await prisma.order.count({
+      where: {
+        adminid: req.params.id,
+        isBilled: true,
+        isServed: true,
+        isCompleted: true,
+      },
+    });
+
+    res.json({ data: count });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
