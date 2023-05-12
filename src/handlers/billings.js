6c8dc93f13abbbe7d45426869d650895c517dee4
Hemantbam <107202719+Hemantbam@users.noreply.github.com>
2023-05-12 14:56:18 +0545

    Update price of order in billings.js

--- src/handlers/billings.js
+++ src/handlers/billings.js
@@ -5,7 +5,7 @@ export const createBill = async (req, res) => {
     const { id, table_number, price, timestamp } = req.body;
     fs.writeFileSync(
       `./src/bills/bill-${id}.txt`,
-      `Bill for:\nTable Number: ${table_number}\nCreate at ${timestamp}`
+      `Bill for:\nTable Number: ${table_number}\nCreate at ${timestamp}\nThe total amount: ${price}`
     );
     res.json({ message: "done" });
   } catch (error) {
